af08bfc1c44cd4ec9e073f559876d5961d30c7fe
Damon River <damonriver@fastmail.com>
2025-05-05 12:20:20 -0500

    Update bjw-s/app-template chart
    Signed-off-by: Damon River <damonriver@fastmail.com>

--- charts/bjw-s/app-template/default.nix
+++ charts/bjw-s/app-template/default.nix
@@ -1,6 +1,6 @@
 {
-  repo = "https://bjw-s.github.io/helm-charts/";
+  repo = "https://bjw-s-labs.github.io/helm-charts/";
   chart = "app-template";
   version = "3.7.3";
-  chartHash = "sha256-ZkgsF4Edl+s044BR4oQIXDS3S6pT/B8V3TEjDQzx6eE=";
+  chartHash = "sha256-9r+yJp3CpF19GMFwATEvksEvRVQs46v/VTiZxUba/ec=";
 }
