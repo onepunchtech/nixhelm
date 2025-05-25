49c58cb7aa263936c6a1ecb5ec49bf9d1eb7f212
Damon River <damonriver@fastmail.com>
2025-05-25 15:21:11 -0500

    concourse/concourse: init at 18.1.4

---
+++ charts/concourse/concourse/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://concourse-charts.storage.googleapis.com/";
+  chart = "concourse";
+  version = "18.1.4";
+  chartHash = "sha256-Pcinrtd/1fuve/TSU7sM2iI2mfmQ9YB9x4uj5V4AVkw=";
+}
